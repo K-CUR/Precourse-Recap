1167d237669c9e68e30219abc724e43aea50d5e5
Katy Currie <katylcurrie@gmail.com>
2021-03-08 21:31:47 +0000

    added guessing game

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,1 +1,9 @@
-I'll come back to the guessing game
+popular_breed="Labrador"
+guess=input("What is the UK's most popular breed of dog?")
+while guess != popular_breed:
+  print("Try again")
+  guess = input()
+else:
+    print("You are correct!")
+
+
